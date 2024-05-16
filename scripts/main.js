7c8a17b06128264610fded3df304d7e877066752
atikur-24 <atikurrahman7223@gmail.com>
2024-05-16 08:35:25 +0600

    Success Stories and footer sections

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+// Fetch the JSON data
+fetch("/data/success.json")
+  .then((response) => response.json())
+  .then((data) => {
+    displayStories(data);
+  })
+  .catch((error) => console.error("Error fetching data:", error));
+
+function displayStories(data) {
+  const storiesContainer = document.getElementById("stories-container");
+
+  data.forEach((story) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+
+    div.innerHTML = `
+          <div class="card border-0 story-card h-100">
+            <img src="${story.img}" class="card-img-top" alt="${story.title}">
+            <div class="card-body text-left">
+              <h6 class="card-title">${story.title}</h6>
+              <p class="card-text">${story.desc}</p>
+            </div>
+          </div>
+        `;
+
+    storiesContainer.appendChild(div);
+  });
+}
